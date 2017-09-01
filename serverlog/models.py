30a5960964eac04970cfcf103463511e6b8278ca
root <root@rescue>
2017-09-01 02:29:33 +0200

    update serverlog app for new django version

--- serverlog/models.py
+++ serverlog/models.py
@@ -1,9 +1,9 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from django.db import models
 
 class InstanceLog(models.Model):
     message = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
-    def __unicode__(self):
-        return self.message
