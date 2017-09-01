0ed2b009db3997c79b4e0bd5b9725fd5bf4a7952
root <root@rescue>
2017-09-01 02:26:04 +0200

    update secrets app for new django version

--- secrets/views.py
+++ secrets/views.py
@@ -1,22 +1,16 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect
-from django.template import RequestContext
-from django.core.urlresolvers import reverse
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
 
 from servers.models import Compute
 from secrets.forms import AddSecret
-
 from vrtManager.secrets import wvmSecrets
-
 from libvirt import libvirtError
 
-
 def secrets(request, host_id):
-    """
-    Networks block
-    """
     if not request.user.is_authenticated():
-        return HttpResponseRedirect(reverse('login'))
+        return redirect('login')
 
     errors = []
     secrets_all = []
@@ -45,17 +39,17 @@ def secrets(request, host_id):
                 if form.is_valid():
                     data = form.cleaned_data
                     conn.create_secret(data['ephemeral'], data['private'], data['usage_type'], data['data'])
-                    return HttpResponseRedirect(request.get_full_path())
+                    return redirect(request.get_full_path())
             if 'delete' in request.POST:
                 uuid = request.POST.get('uuid', '')
                 conn.delete_secret(uuid)
-                return HttpResponseRedirect(request.get_full_path())
+                return redirect(request.get_full_path())
             if 'set_value' in request.POST:
                 uuid = request.POST.get('uuid', '')
                 value = request.POST.get('value', '')
                 conn.set_secret_value(uuid, value)
-                return HttpResponseRedirect(request.get_full_path())
+                return redirect(request.get_full_path())
     except libvirtError as err:
         errors.append(err)
 
-    return render_to_response('secrets.html', locals(), context_instance=RequestContext(request))
+    return render(request, 'secrets.html', locals())
